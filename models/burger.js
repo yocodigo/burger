4e337722bc818592c135e6df55d73185bcd4823a
unknown <jjhernandez94@gmail.com>
2019-03-14 12:04:10 -0700

    UPDATE
    Added concurrently to package.json script

--- models/burger.js
+++ models/burger.js
@@ -1,6 +1,6 @@
 // Import the ORM to create functions that will interact with the database.
 const orm = require("../config/orm.js"),
-const burger = {
+burger = {
   all: function(cb) {
     orm.selectAll("burger", function(res) {
       cb(res);
